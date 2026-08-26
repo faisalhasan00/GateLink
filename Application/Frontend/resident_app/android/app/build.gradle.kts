35cf8d336f5bfba4ca21548870576744dadc5d91
Faisal Hasan <mohammedfaisalhasan@gmail.com>
2026-08-26 14:35:42 +0530

    chore(resident_app): bump versionCode to 7 (version 1.0.6+7)

--- Application/Frontend/resident_app/android/app/build.gradle.kts
+++ Application/Frontend/resident_app/android/app/build.gradle.kts
@@ -22,8 +22,8 @@ if (localPropertiesFile.exists()) {
     localPropertiesFile.inputStream().use { localProperties.load(it) }
 }
 
-val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toInt() ?: 6
-val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0.5"
+val flutterVersionCode = localProperties.getProperty("flutter.versionCode")?.toInt() ?: 7
+val flutterVersionName = localProperties.getProperty("flutter.versionName") ?: "1.0.6"
 
 android {
     namespace = "in.gatelink.app"
